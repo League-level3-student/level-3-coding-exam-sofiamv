31fb868770bd0c98f6acf0ec45a5f7e25434dc0b
League Student <league@iMac-27.attlocal.net>
2019-08-24 15:58:09 -0700

    level 3 coding exam
    gdrtasgsdasfd

---
+++ src/HashMapCalculator.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class HashMapCalculator {
+
+	public int commonKeyValuePairs(HashMap<String, String> hash, HashMap<String, String> map) {
+		for(int i = 0; i < hash.size(); i++) {
+			System.out.println(hash.values());
+		}
+		return 0;
+	}
+}
--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
 
 public class VoteProcessor {
 
+	public String calculateElectionWinner(ArrayList<String> a) {
+		int c = 0;
+		int b = 0;
+		String winner = "";
+		for(int i = 0; i < a.size(); i++) {
+			if(a.get(i).equalsIgnoreCase("pope francis")) {
+				c++;
+			} else if (a.get(i).equalsIgnoreCase("edward snowden")) {
+				b++;
+			}
+		}if(c > b) {
+			winner = "pope francis";
+		}else if (b > c) {
+			winner = "edward snowden";
+		}else if (b == c) {
+			winner = "TIE";
+		}
+		return winner;
+	}
 }
